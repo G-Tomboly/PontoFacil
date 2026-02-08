eca676a0683b5632df2a19f0e42e770ca07c59c1
Guilherme Tomboly Borges <tomboly.academico@gmail.com>
2026-02-07 23:55:11 -0300

    Corrigindo Bugs para Render

--- sistema-ponto/backend/server.js
+++ sistema-ponto/backend/server.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const database = require('./database');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(cors());
